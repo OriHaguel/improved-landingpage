01416ce2d1d6506796ae29da644b59138112dcec
OriHaguel <orihaguel@gmail.com>
2025-02-27 15:52:08 +0200

    Add smooth scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,23 @@ import { Projects } from "@/cmps/Projects";
 import { ProjectsParallax } from "@/cmps/ProjectsParallax";
 import StyledText from "@/cmps/TextEffect";
 import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 const HomePage = () => {
+    useEffect(() => {
+
+        const lenis = new Lenis({
+            lerp: 0.1,
+
+        })
+        function raf(time: any) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+        requestAnimationFrame(raf)
+    }, [])
+
     return (
         <div>
 
