f731792022910b9a1ed10729a350951ea84809e8
OriHaguel <orihaguel@gmail.com>
2025-03-05 14:25:52 +0200

    Remove deprecated image assets and update ProjectsParallax to use webp format for improved performance

--- src/cmps/ProjectsParallax.tsx
+++ src/cmps/ProjectsParallax.tsx
@@ -1,17 +1,18 @@
 import { Tilt } from "./TiltEffect";
-import gighub from '../../src/assets/img/gighub.png'
+import gighub from '../../src/assets/img/gighub.webp'
+import fitrack from '../../src/assets/img/fitrack.webp'
 import blueMountain from '../assets/img/blueimountain.webp'
 
 export function ProjectsParallax() {
 
     const projects = [
+
+        {
+            image: fitrack,
+        },
         {
             image: gighub,
         },
-        {
-            image: blueMountain,
-        },
-
     ]
 
     return (
@@ -68,14 +69,14 @@ export function ProjectsParallax() {
                             >
                             </div> */}
 
-            <div className='parallax-main'>
+            {/* <div className='parallax-main'>
                 <div className='absolute inset-0 w-full h-full  overflow-hidden'>
                     <div className='parallax-effect-div bg-cover bg-no-repeat bg-center w-[100vw] h-[100vh] -z-1 bg-blur '>
                         <div className=' absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                             hellohgffhgfhgfhfhfghfg</div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
         </section>
     )
