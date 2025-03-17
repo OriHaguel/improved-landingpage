412747ee1f5b49003b3b89b83e3a504091b8b6fa
OriHaguel <orihaguel@gmail.com>
2025-03-17 16:06:45 +0200

    Uncomment components in HomePage for improved layout visibility

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,10 +27,9 @@ const HomePage = () => {
 
     return (
         <div>
-            {/* <Mainpage /> */}
-            {/* <AboutCards /> */}
-            {/* <MaskHover /> */}
-            {/* <Projects /> */}
+            <Mainpage />
+            <MaskHover />
+            <Projects />
             {/* <FormMsg /> */}
         </div>
     )
